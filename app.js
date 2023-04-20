f4f20772b5c2b0910b89f1438455cc69c4fc31c3
Yanting Lai <laiyanting.neu@gmail.com>
2023-04-19 23:13:43 -0700

    update

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 const express = require('express');
 const app = express();
 const port = 3003;
-const middleware = require('./middleware')
-const path = require('path')
-const bodyParser = require("body-parser")
-const mongoose = require("./database");
-const session = require("express-session");
+const middleware = require('./middleware');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('./database');
+const session = require('express-session');
 
-const server = app.listen(port, () => console.log("Server listening on port " + port));
+const server = app.listen(port, () =>
+  console.log('Server listening on port ' + port)
+);
 
-app.set("view engine", "pug");
-app.set("views", "views");
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({
-    secret: "bbq chips",
+app.use(
+  session({
+    secret: 'bbq chips',
     resave: true,
-    saveUninitialized: false
-}))
+    saveUninitialized: false,
+  })
+);
 
 // Routes
 const loginRoute = require('./routes/loginRoutes');
@@ -29,27 +33,26 @@ const profileRoute = require('./routes/profileRoutes');
 const uploadRoute = require('./routes/uploadRoutes');
 const searchRoute = require('./routes/searchRoutes');
 
-// Api routes
+// Api routes for difference pages
 const postsApiRoute = require('./routes/api/posts');
 const usersApiRoute = require('./routes/api/users');
 
-app.use("/login", loginRoute);
-app.use("/register", registerRoute);
-app.use("/posts", middleware.requireLogin, postRoute);
-app.use("/profile", middleware.requireLogin, profileRoute);
-app.use("/uploads", uploadRoute);
-app.use("/search", middleware.requireLogin, searchRoute);
+app.use('/login', loginRoute);
+app.use('/register', registerRoute);
+app.use('/posts', middleware.requireLogin, postRoute);
+app.use('/profile', middleware.requireLogin, profileRoute);
+app.use('/uploads', uploadRoute);
+app.use('/search', middleware.requireLogin, searchRoute);
 
-app.use("/api/posts", postsApiRoute);
-app.use("/api/users", usersApiRoute);
+app.use('/api/posts', postsApiRoute);
+app.use('/api/users', usersApiRoute);
 
-app.get("/", middleware.requireLogin, (req, res, next) => {
+app.get('/', middleware.requireLogin, (req, res, next) => {
+  var payload = {
+    pageTitle: 'Home',
+    userLoggedIn: req.session.user,
+    userLoggedInJs: JSON.stringify(req.session.user),
+  };
 
-    var payload = {
-        pageTitle: "Home",
-        userLoggedIn: req.session.user,
-        userLoggedInJs: JSON.stringify(req.session.user),
-    }
-
-    res.status(200).render("home", payload);
-})
+  res.status(200).render('home', payload);
+});
